09154691290a213a6348e112a95df1354cc3c66c
Nivia Nur Kharisma <53246336+NiviaNurKharisma@users.noreply.github.com>
2019-10-09 08:30:27 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,46 @@
+<?php
+//echo "Hello World !";
+//echo "<h1>Hallo Dunia !</h1>";
+//VARIABLE
+//diawali deengan tanda nama
+//nama variable tidak boleh diawali dengan angka
+//boleh mengandung angka
+//$nama = "Nivia Nur Kharisma";
+//$pesan = "Hallo, Selamat datang <strong>$nama</strong>";
+
+//echo $pesan;
+
+//OPETATOR
+//aritmatika
+//+ - * / %
+//$a = 10;
+//$b = 20;
+//echo $a+$b;
+
+//CONCATENATION /CONCAT / PENGGABUNG STRING
+//.
+//$nama_depan = "Nivia";
+//$nama_belakang = "Nur Kharisma";
+//echo $nama_depan. " " . $nama_belakang;
+
+//ASSIGMENT / PENUGASAN
+//$x = "Nivia";
+//$x .= " ";
+//$x .= "Nur Kharisma";
+//echo $x;
+
+//echo "<br>";
+//$angka = 10;
+//$angka += 5;
+//$angka += 3;
+//echo $angka;
+
+//PERBANDINGAN / COMPARATION
+//==, <=, >=, <, >, !=
+
+//IDENTITAS / STRICT COMPARATION
+//===, !==
+
+//LOGIKA / LOGICAL
+// &&, ||, !
+?>
